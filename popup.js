7ac925831300f6b935f5c047b6d77967b2a24b05
Freyam Mehta <frey77mehta@gmail.com>
2021-08-01 17:52:06 +0400

    Clean

--- popup.js
+++ popup.js
@@ -1,25 +1,19 @@
-// url Async requesting function
 function httpGetAsync(theUrl, callback) {
-  // create the request object
   var xmlHttp = new XMLHttpRequest();
 
-  // set the state change callback to capture when the response comes in
   xmlHttp.onreadystatechange = function () {
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
       callback(xmlHttp.responseText);
     }
   };
 
-  // open as a GET call, pass in the url and set async = True
   xmlHttp.open("GET", theUrl, true);
 
-  // call send with no params as they were passed in on the url string
   xmlHttp.send(null);
 
   return;
 }
 
-// hide banner for 2 seconds
 setTimeout(function () {
   document.getElementById("banner").style.display = "none";
 }, 1000);
